c18b8d7e4c5277b124b0d4f69d9b3fcdb0a484d1
idlkz <idlkz2019@gmail.com>
2022-02-17 00:34:54 +0600

    1

---
+++ plugins/calendar.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Vuetify from "vuetify";
+import DaySpanVuetify from 'dayspan-vuetify'
+
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'dayspan-vuetify/dist/lib/dayspan-vuetify.min.css'
+
+Vue.use(Vuetify)
+Vue.config.productionTip = false
+
+Vue.use(DaySpanVuetify);
